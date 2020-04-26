2a3937831649a08dc0399181f2e06f4418770128
Christoph Geypen <geypenchristoph@gmail.com>
2020-04-26 14:46:05 +0200

    get barchart working

--- src/index.js
+++ src/index.js
@@ -1,20 +1,47 @@
+const { GoogleCharts } = require('google-charts');
 const dscc = require('@google/dscc');
-const viz = require('@google/dscc-scripts/viz/initialViz.js');
 const local = require('./localMessage.js');
 
 // change this to 'true' for local development
 // change this to 'false' before deploying
 export const LOCAL = true;
 
-// write viz code here
-const drawViz = (data) => {
-  viz.readmeViz();
-  viz.firstViz(data);
-};
+// Load the charts library with a callback
+GoogleCharts.load(drawChart);
+document.body.innerHTML += '<div id="chart1"></div>';
+
+function drawChart(test) {
+  console.log(GoogleCharts);
+
+  const margin = {
+    left: 20, right: 20, top: 20, bottom: 20,
+  };
+  const height = dscc.getHeight() - 10;
+  const width = dscc.getWidth();
+
+  console.log(height);
+
+  const chartHeight = height - margin.top - margin.bottom;
+  const chartWidth = width - margin.left - margin.right;
+  // Standard google charts functionality is available as GoogleCharts.api after load
+  const data = GoogleCharts.api.visualization.arrayToDataTable([
+    ['Chart thing', 'Chart amount'],
+    ['Lorem ipsum', 60],
+    ['Dolor sit', 22],
+    ['Sit amet', 18],
+  ]);
+  const pie_1_chart = new GoogleCharts.api.visualization.PieChart(document.getElementById('chart1'));
+  pie_1_chart.draw(data);
+}
+
+// const drawViz = (data) => {
+// viz.readmeViz();
+// viz.firstViz(data);
+// };
 
 // renders locally
 if (LOCAL) {
-  drawViz(local.message);
+  drawChart(local.message);
 } else {
-  dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
+  dscc.subscribeToData(drawChart, { transform: dscc.objectTransform });
 }
